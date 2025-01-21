228828ffe0527482d7c5eb8ed308e5b7d67b1a31
Vladislav Samura <vx5080@yandex.ru>
2025-01-21 13:39:31 +0900

    Добавил две программы из книги

---
+++ guess.py
@@ -0,0 +1,43 @@
+# Это игра по угадыванию чисел
+import random
+
+def getNumber():
+    while type:
+        getNumber = input()                 
+        try:                                   
+            getTempNumber = int(getNumber)
+        except ValueError:                      
+            print('"' + getNumber + '"' + ' - не является числом!')
+            print('Надо ввести число что бы угадать!')
+        else:                                   
+            break
+    return int(getNumber)                   
+
+
+guessesTaken = 0
+print('Привет! Как тебя зовут?')
+myName = input()
+number = random.randint(1, 100)
+print('Что ж, ' + myName + ', я загадываю число от 1 до 100.')
+print('Попробуй угадать!')
+
+for guessesTaken in range(6):
+	guess = getNumber()
+	if guess < number:
+			print('Твое число слишком маленькое.\nПопробуй ещё раз!')
+	if guess > number:
+			print('Твое число слишком большое.\nПопробуй ещё раз!')
+	if guess == number:
+			break
+
+if guess == number:
+	guessesTaken = guessesTaken + 1
+	declension = 'попытки!'
+	if guessesTaken == 1:
+		declension = 'попытку!'
+	elif guessesTaken > 4:
+		declension = 'попыток!'
+	print('Отлично, ' + myName + '!')
+	print('Ты справился за ' + str(guessesTaken) + ' ' + declension)
+else:
+	print('Увы, но попытки кончились!\n Я загадал число ' + str(number) + '.')
